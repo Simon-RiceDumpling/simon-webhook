9dbc9c043c6260e9c6b03277da473a375dcec53d
renbo <simon@atlasmobi.com>
2025-11-28 18:56:28 +0800

    23

--- src/main/java/org/simon/webhook/server/email/EmailSendServiceImpl.java
+++ src/main/java/org/simon/webhook/server/email/EmailSendServiceImpl.java
@@ -1,6 +1,7 @@
 package org.simon.webhook.server.email;
 
 import com.alibaba.fastjson2.JSONObject;
+import jakarta.annotation.Resource;
 import org.simon.webhook.server.ForwardMessageSendService;
 import org.simon.webhook.server.feishu.builder.FeiShuMarkdownBuilder;
 import org.simon.webhook.vo.ExecuteResult;
@@ -13,17 +14,16 @@ import org.springframework.stereotype.Service;
  * @create: 2025-11-28 18:44
  **/
 @Service
-public class EmailSendService implements ForwardMessageSendService<JSONObject, JSONObject> {
-
-    private
+public class EmailSendServiceImpl implements ForwardMessageSendService<JSONObject, JSONObject> {
+    @Resource
+    private EmailService emailService;
 
     @Override
     public ExecuteResult<JSONObject> send(JSONObject params) {
         //利用飞书告警模板组装消息
         FeiShuMarkdownBuilder shuMarkdownBuilder = FeiShuMarkdownBuilder.create("simon webhook message");
         params.forEach(shuMarkdownBuilder::add);
-        String message = shuMarkdownBuilder.build();
-
+        emailService.sendSimpleEmail("18189270679@163.com", "simon webhook message", shuMarkdownBuilder.build());
         return null;
     }
 }
